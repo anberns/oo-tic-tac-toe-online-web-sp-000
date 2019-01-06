3afc482fd7c6d312e3a47996c2231c0197fdc0ca
Aaron Berns <anberns@gmail.com>
2019-01-06 15:50:42 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -40,7 +40,8 @@ class TicTacToe
   end
   
   # valid_move?
-  def valid_move?(board, index)
-    index.between?(0,8) && !position_taken?(board, index)
+  def valid_move?(index)
+    index.between?(0,8) && !position_taken?(@board, index)
   end
+  
 end
