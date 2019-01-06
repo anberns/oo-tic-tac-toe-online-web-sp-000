a8d400b59b2ab85cc9fca7a4f5a7bae0d8be36a5
Aaron Berns <anberns@gmail.com>
2019-01-06 15:53:24 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -57,5 +57,14 @@ class TicTacToe
     end
   end
   
-  
+  # turn_count
+  def turn_count(board)
+    count = 0 
+    board.each do |el|
+      if el == "X" || el == "O"
+        count += 1
+      end
+    end
+    count
+  end
 end
